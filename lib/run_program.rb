99c4ad1b04fd1b1567faf1837263b53b87983899
Abhishek Goswami <abhishekgoswami@Abhisheks-MacBook-Pro.local>
2020-12-09 09:05:31 +0530

    refactor takeOrder class

--- lib/run_program.rb
+++ lib/run_program.rb
@@ -1,22 +1,19 @@
 require_relative "manage_order"
 
 class TakeOrder
-  def initialize
-    @items = gets.chomp
-  end
-
-  def order
+  def self.manage
     puts "Please enter all the items pusrchased separated by a comma"
-items = gets.chomp
-ordered_items = ManageOrder.new(items)
-ordered_items.show_bill
+    items = gets.chomp
+    if(items.strip.length != 0)
+      ManageOrder.new(items).show_bill
+    else
+      puts "Aww!! It seems you didn't enter any item"
+      manage
+    end
   end
 end
 
-# puts "Please enter all the items pusrchased separated by a comma"
-# items = gets.chomp
-# ordered_items = ManageOrder.new(items)
-# ordered_items.show_bill
+TakeOrder.manage
 
+# milk,milk, bread,banana,bread,bread,bread,milk,apple
 
-  # "milk,milk, bread,banana,bread,bread,bread,milk,apple"
